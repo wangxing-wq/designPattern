8362634b8188a194b80935ca466f7d2fa5eaafeb
weishengwx <2234346829@qq.com>
2022-10-10 07:01:09 +0800

    1.2 目前在REMADE.MD里面写了自己对工厂模式,抽象工厂模式,建造者模式,策略模式的理解

---
+++ src/main/java/com/wx/abstractFactoryPattern/AbstractFactoryPattern.java
@@ -0,0 +1,9 @@
+package com.wx.abstractFactoryPattern;
+
+/**
+ * @author 22343
+ * @version 1.0
+ * 抽象工厂,感觉适合对原有结构的优化,对多个不兼容对象进行整合
+ */
+public class AbstractFactoryPattern {
+}
---
+++ src/main/java/com/wx/factoryPattern/pattern/FactoryPattern.java
@@ -0,0 +1,12 @@
+package com.wx.factoryPattern.pattern;
+
+/**
+ * @author 22343
+ * @version 1.0
+ * 模拟发奖服务,修改这个使用适合模式,要查看这个代码如何抽出
+ * 工厂模式注重 构建
+ * 策略模式注重处理,其实抽奖这个我更觉得适合用策略模式.
+ * 如果是生成数据连接的我觉得更适合工厂模式
+ */
+public class FactoryPattern {
+}
